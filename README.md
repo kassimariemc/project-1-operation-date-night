9791dae0e656226e62441b079803a525142957f1
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-08-05 17:15:24 -0400

    added restaurant photo

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A simple app that helps you enjoy a night at home with dinner and a movie, so yo
 * responsive for all screen sizes
 * Bootstrap components
 * dynamically updated HTML and CSS powered by jQuery
-* [The Movie Database API](https://developers.themoviedb.org/3/getting-started/introduction)
+* [The Movie Database API](https://developers.themoviedb.org/3/getting-started/introduction) + [The Open Movie Database](https://developers.themoviedb.org/3/getting-started/introduction))
 * [Google Places API](https://developers.google.com/places/web-service/overview)
 
 ## Deployment
@@ -15,3 +15,10 @@ A simple app that helps you enjoy a night at home with dinner and a movie, so yo
 [link to deployment](https://kassimariemc.github.io/project-1-operation-date-night/)
 
 
+## Contributors
+
+<a href="https://github.com/ivybean"><img src="https://avatars3.githubusercontent.com/u/66763597?s=460&v=4" width="5%" style="border-radius:50%"> @ivybean</a>
+
+<a href="https://github.com/jhf1203"><img src="https://avatars1.githubusercontent.com/u/66701459?s=460&u=aca904c0faf90f8366007595d78cb02f850e05c7&v=4" width="5%" style="border-radius:50%"> @jhf1203</a>
+
+<a href="https://github.com/kassimariemc"><img src="https://avatars3.githubusercontent.com/u/66877217?s=460&u=2aefa4a92adc823f1df8656008e78fe8edd0f9b6&v=4" width="5%" style="border-radius:50%"> @kassimariemc</a>
--- assets/script.js
+++ assets/script.js
@@ -1,200 +1,184 @@
-//RESTAURANT GENERATOR
+$(document).ready(function () {
 
-//SETUP Variables
-//================================================
-var authKey= "&key=AIzaSyAApQZVRRj-sNhF5LM9eZVQM8qOii5Orq4";
+  //RESTAURANT GENERATOR
 
-var cuisine= "";
-var userCity= "";
-var userState= "";
-var radius= 0;
+  //SETUP Variables
+  //================================================
+  var authKey = "&key=AIzaSyAApQZVRRj-sNhF5LM9eZVQM8qOii5Orq4";
 
-// URL Base
-var queryURLBase = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+  // URL Base
+  var queryURLBase = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
 
-//Functions
-//================================================
+  //Functions
+  //================================================
 
-function runQuery(rQueryURL){
+  function runQuery(rQueryURL) {
 
-  //AJAX Function
-  $.ajax({
-    url: rQueryURL,
-    method: "GET"})
-  // .done(function(placesData){
-  //   console.log(rQueryURL);
-  //   console.log(placesData);
-  // })
-  .then(function(placesData){
-    
-    for (var i=0; i<3; i++){
-     
-        getRestaurantDetails (placesData.results[i].place_id);  
-    }
-  })
-};
+    //AJAX Function
+    $.ajax({
+      url: rQueryURL,
+      method: "GET"
+    })
+      .then(function (placesData) {
 
-//Function to Call for restaurant Details
-function getRestaurantDetails(restaurant) {
-$.ajax({
-  url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=" + restaurant + "&fields=name,formatted_address,url,website,rating,formatted_phone_number" + authKey,
-  method: "GET"}).then(function(placesData2){
- 
+        var j = Math.floor(Math.random() * (placesData.results.length - 4));
+
+        // console.log(response.results.length + " is how many we have to choose from");
+        for (var i = j; i < (j + 3); i++){
+          getRestaurantDetails(placesData.results[i].place_id);
+        }
+      })
+  };
+
+  //Function to Call for restaurant Details
+  function getRestaurantDetails(restaurant) {
+    $.ajax({
+      url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=" + restaurant + "&fields=name,photo,formatted_address,url,website,rating,formatted_phone_number" + authKey,
+      method: "GET"
+    }).then(function (placesData2) {
+      console.log(placesData2);
       //Restaurant Name
-       var restaurantName = placesData2.result.name;
-       console.log(restaurantName);
-       var restaurantNameCard = $("<h4>").html(restaurantName).attr("class", "rest-title");
+      var restaurantName = placesData2.result.name;
+      var restaurantNameCard = $("<h4>").html(restaurantName).attr("class", "rest-title");
 
-       //Restaurant Address
-       var restaurantAddress = placesData2.result.formatted_address;
-        console.log(restaurantAddress);
-        var restaurantAddressCard = $("<p>").html("Address: " + restaurantAddress).attr("class", "p-address");
+      //Restaurant Photo
+      var restaurantPhotoCard = $("<img>").attr("src", "https://maps.googleapis.com/maps/api/place/photo?photoreference=" + placesData2.result.photos[0].photo_reference + "&sensor=false&maxheight=200&maxwidth=200" + authKey)
+
+      //Restaurant Address
+      var restaurantAddress = placesData2.result.formatted_address;
+      var restaurantAddressCard = $("<p>").html("Address: " + restaurantAddress).attr("class", "p-address");
 
       //Restaurant Number
       var restaurantPhone = placesData2.result.formatted_phone_number;
-      console.log(restaurantPhone)
       var restaurantPhoneCard = $("<p>").html("Phone: " + restaurantPhone).attr("class", "p-phone");
 
       //Restaurant Rating
       var restaurantRating = placesData2.result.rating
-      console.log(restaurantRating);
       var restaurantRatingCard = $("<p>").html("Rating: " + restaurantRating + "/5").attr("class", "p-address");
 
       //Restaurant GoogleMaps Link      
-      var restaurantMap =  $("<a>").text("Google Maps").attr("href", placesData2.result.url);
-  
+      var restaurantMap = $("<a>").text("Google Maps").attr("href", placesData2.result.url).attr("target", "_blank");
+
       //Restaurant Website Link
-      var restaurantURL =  $("<a>").text("Restaurant Website").attr("href", placesData2.result.website);
+      var restaurantURL = $("<a>").text("Restaurant Website").attr("href", placesData2.result.website).attr("target", "_blank");
 
       //Restaurant Cards
       var restaurantRow = $(".restaurant-row");
       var restaurantCol = $("<div>").attr("class", "col-lg-4 restaurant-col");
-      var restaurantCard = $("<div>").attr("class", "card restaurant-card");
+      var restaurantCard = $("<div>").attr("class", "card restaurant-card").attr("style", "height: 500px;");
       var restaurantCardHeader = $("<div>").attr("class", "card-header restaurant-card-header");
       var restaurantCardBody = $("<div>").attr("class", "card-body restaurant-card-body");
 
       restaurantRow.append(restaurantCol.append(restaurantCard));
       restaurantCard.append(restaurantCardHeader.append(restaurantNameCard));
       restaurantCard.append(restaurantCardBody);
-      restaurantCardBody.append(restaurantAddressCard);
-      restaurantCardBody.append(restaurantMap);
-      restaurantCardBody.append(restaurantPhoneCard);
-      restaurantCardBody.append(restaurantRatingCard);
-      restaurantCardBody.append(restaurantURL);
-    // }
-  });
-};
-
-//MAIN PROCESS
-//================================================
-
-$('#rest-btn').on('click', function(){
-
-  //Get Cuisine
-  var cuisine = $('#rest-cuisine').val();
-
-  //Add Cuisine
-  if (cuisine == "any"){
-    var restURL = queryURLBase + "restaurants+in+"; 
-  } else {
-    var restURL = queryURLBase + cuisine + "+restaurants+in+";
+      restaurantCardBody.append(restaurantPhotoCard, restaurantAddressCard, restaurantMap, restaurantPhoneCard, restaurantRatingCard, restaurantURL);
+     
+    });
   };
 
-  //Get City
-  var userCity = $('#rest-city').val().trim();
-  //Add in the City
-  var restURL = restURL + userCity;
-  //Get State
-  var userState = $('#rest-state').val();
-  //Add in the State
-  var restURL = restURL + "+" + userState;
-  //Get Radius
-  var radius = $('#rest-radius').val() * 1609;
-  //Add in the radius
-  var restURL = restURL + "&radius=" + radius
-  //Add API Key
-  var restURL = restURL + authKey;
-  console.log(restURL);
-  //Send the AJAX Call the newly assembled URL
-  runQuery(restURL);
-});
+  //MAIN PROCESS
+  //================================================
+
+  $('#rest-btn').on('click', function () {
+
+    //Get Cuisine
+    var cuisine = $('#rest-cuisine').val();
+
+    //Add Cuisine
+    if (cuisine == "any") {
+      var restURL = queryURLBase + "restaurants+in+";
+    } else {
+      var restURL = queryURLBase + cuisine + "+restaurants+in+";
+    };
+
+    //Get City
+    var userCity = $('#rest-city').val().trim();
+    //Add in the City
+    var restURL = restURL + userCity;
+    //Get State
+    var userState = $('#rest-state').val();
+    //Add in the State
+    var restURL = restURL + "+" + userState;
+    //Get Radius
+    var radius = $('#rest-radius').val() * 1609;
+    //Add in the radius
+    var restURL = restURL + "&radius=" + radius
+    //Add API Key
+    var restURL = restURL + authKey;
+    console.log(restURL);
+    //Send the AJAX Call the newly assembled URL
+    runQuery(restURL);
+  });
 
 
 
 
-//MOVIE GENERATOR
+  //MOVIE GENERATOR
+  $("#movie-btn").on("click", function () {
 
+    // Assigning variables to our user-selected search criteria
+    var startYear = $(".earliest-year-selector").val();
+    var endYear = $(".latest-year-selector").val();
+    var genre = $("#genre-input").val();
 
-$("#movie-btn").on("click", function () {
-
-  // Assigning variables to our user-selected search criteria
-  var startYear = $(".earliest-year-selector").val();
-  var endYear = $(".latest-year-selector").val();
-  var genre = $("#genre-input").val();
-  
-  // Ajax call to retrieve movie titles based off of above criteria (tmdb)
-  $.ajax({
+    // Ajax call to retrieve movie titles based off of above criteria (tmdb)
+    $.ajax({
       url: "https://api.themoviedb.org/3/discover/movie?api_key=183cf14b0fa970fabe87a2879d2f3aa1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=" + startYear + "-01-01&primary_release_date.lte=" + endYear + "-12-31&with_genres=" + genre + "&with_original_language=en",
       method: "GET",
-  }).then(function(response) {
-  
+    }).then(function (response) {
+
       // Ensuring we do not always populate the top three results, allowing the user to search multiple times if the three titles that populate do not meet their needs
 
-      var j = Math.floor(Math.random() * response.results.length - 4);
+      var j = Math.floor(Math.random() * (response.results.length - 4));
 
       // console.log(response.results.length + " is how many we have to choose from");
       for (var i = j; i < (j + 3); i++) {
-      var tmdbFullYear = response.results[i].release_date;
-      var tmdbYearOnly = tmdbFullYear.substring(0,4);
-      var title = response.results[i].original_title;
-      // console.log(response);
-      // console.log("index " + i + " "  + title + " is the tmdb title");
-      // Clearing out previous movies generated
-      $(".movie-col").remove();
+        var tmdbFullYear = response.results[i].release_date;
+        var tmdbYearOnly = tmdbFullYear.substring(0, 4);
+        var title = response.results[i].original_title;
+        // console.log(response);
+        // console.log("index " + i + " "  + title + " is the tmdb title");
+        // Clearing out previous movies generated
+        $(".movie-col").remove();
 
-      // Setting parameters for second ajax call (omdb)
-      var param = $.param({
+        // Setting parameters for second ajax call (omdb)
+        var param = $.param({
           t: title,
           y: tmdbYearOnly,
           plot: "short",
           apikey: "dadc91b3"
         });
-    
+
         $.ajax({
-            url:"https://www.omdbapi.com/?" + param,
-            method: "GET", 
-        }).then(function(movie) {
-    
+          url: "https://www.omdbapi.com/?" + param,
+          method: "GET",
+        }).then(function (movie) {
+
           // console.log(movie.Title + " is the movie name in omdb");
           // Generating card elements for each movie chosen.
-            var movieRow = $(".movie-row");
-            var movieCol = $("<div>").attr("class", "col-lg-4 movie-col");
-            var movieCard = $("<div>").attr("class", "card movie-card");
-            var movieCardHeader = $("<div>").attr("class", "card-header movie-card-header");
-            var movieCardBody = $("<div>").attr("class", "card-body movie-card-body");
-            var movieName = $("<h4>").html(movie.Title).attr("class", "movie-title");
-            var movieRating = $("<p>").html("Rated " + movie.Rated).attr("class", "p-rated");
-            var movieCast = $("<p>").html("Starring | " + movie.Actors).attr("class", "p-cast");
-            var moviePlot = $("<p>").html(movie.Plot).attr("class", "p-plot");
-            var movieTrailer = $("<a>").attr("href", "https://www.youtube.com/results?search_query=" + movie.Title + "+trailer").attr("target", "_blank").html("View trailers here");
-            var linkSeparator = $("<div>");
-            var movieStreams = $("<a>").attr("href", "https://www.justwatch.com/us/search?q=" + movie.Title).attr("target", "_blank").html("Where to find it");
-    
-            movieRow.append(movieCol.append(movieCard));
-            movieCard.append(movieCardHeader.append(movieName));
-            movieCard.append(movieCardBody);
-            movieCardBody.append(movieRating);
-            movieCardBody.append(movieCast);
-            movieCardBody.append(moviePlot);
-            movieCardBody.append(movieTrailer);
-            movieCardBody.append(linkSeparator);
-            movieCardBody.append(movieStreams);
-    
-    
-          
-          })
-        }
-      });
+          var movieRow = $(".movie-row");
+          var movieCol = $("<div>").attr("class", "col-lg-4 movie-col");
+          var movieCard = $("<div>").attr("class", "card movie-card");
+          var movieCardHeader = $("<div>").attr("class", "card-header movie-card-header");
+          var movieCardBody = $("<div>").attr("class", "card-body movie-card-body");
+          var movieName = $("<h4>").html(movie.Title).attr("class", "movie-title");
+          var movieRating = $("<p>").html("Rated " + movie.Rated).attr("class", "p-rated");
+          var movieCast = $("<p>").html("Starring | " + movie.Actors).attr("class", "p-cast");
+          var moviePlot = $("<p>").html(movie.Plot).attr("class", "p-plot");
+          var movieTrailer = $("<a>").attr("href", "https://www.youtube.com/results?search_query=" + movie.Title + "+trailer").attr("target", "_blank").html("View trailers here");
+          var linkSeparator = $("<div>");
+          var movieStreams = $("<a>").attr("href", "https://www.justwatch.com/us/search?q=" + movie.Title).attr("target", "_blank").html("Where to find it");
+
+          movieRow.append(movieCol.append(movieCard));
+          movieCard.append(movieCardHeader.append(movieName));
+          movieCard.append(movieCardBody);
+          movieCardBody.append(movieRating, movieCast, moviePlot, movieTrailer, linkSeparator, movieStreams);
+
+        })
+      }
+    });
+  });
 });
 
 

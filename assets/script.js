90c3586f3e6030addb8d65e8d53d6921152d8389
Jim Faulkner <jhf1203@gmail.com>
2020-08-04 20:38:34 -0400

    de-bugged movie search function

--- assets/script.js
+++ assets/script.js
@@ -88,77 +88,75 @@ $('#rest-btn').on('click', function(){
 
 $("#movie-btn").on("click", function () {
 
+  // Assigning variables to our user-selected search criteria
+  var startYear = $(".earliest-year-selector").val();
+  var endYear = $(".latest-year-selector").val();
+  var genre = $("#genre-input").val();
+  
+  // Ajax call to retrieve movie titles based off of above criteria (tmdb)
+  $.ajax({
+      url: "https://api.themoviedb.org/3/discover/movie?api_key=183cf14b0fa970fabe87a2879d2f3aa1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=" + startYear + "-01-01&primary_release_date.lte=" + endYear + "-12-31&with_genres=" + genre + "&with_original_language=en",
+      method: "GET",
+  }).then(function(response) {
+  
+      // Ensuring we do not always populate the top three results, allowing the user to search multiple times if the three titles that populate do not meet their needs
+
+      var j = Math.floor(Math.random() * response.results.length-3);
+
+      for (var i = j; i < (j + 3); i++) {
+
+      var tmdbFullYear = response.results[i].release_date;
+      var tmdbYearOnly = tmdbFullYear.substring(0,4);
+      var title = response.results[i].title;
+        
+      // Clearing out previous movies generated
+      $(".movie-col").remove();
+
+      // Setting parameters for second ajax call (omdb)
+      var param = $.param({
+          t: title,
+          y: tmdbYearOnly,
+          plot: "short",
+          apikey: "dadc91b3"
+        });
     
-    var startYear = $(".earliest-year-selector").val();
-    var endYear = $(".latest-year-selector").val();
-    var genre = $("#genre-input").val();
-    console.log("This is the first year " + startYear);
-    console.log("This is the second year" + endYear);
-
-    $.ajax({
-        url: "https://api.themoviedb.org/3/discover/movie?api_key=183cf14b0fa970fabe87a2879d2f3aa1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=" + startYear + "-01-01&primary_release_date.lte=" + endYear + "-12-31&with_genres=" + genre,
-        method: "GET",
-      }).then(function(response) {
-        
-        for (var i = 0; i < 3; i++) {
-
-        getMovieDetails(response.results[i].title)
-
-        
-
+        $.ajax({
+            url:"https://www.omdbapi.com/?" + param,
+            method: "GET", 
+        }).then(function(movie) {
+    
+          // Generating card elements for each movie chosen.
+            var movieRow = $(".movie-row");
+            var movieCol = $("<div>").attr("class", "col-lg-4 movie-col");
+            var movieCard = $("<div>").attr("class", "card movie-card");
+            var movieCardHeader = $("<div>").attr("class", "card-header movie-card-header");
+            var movieCardBody = $("<div>").attr("class", "card-body movie-card-body");
+            var movieName = $("<h4>").html(movie.Title).attr("class", "movie-title");
+            var movieRating = $("<p>").html("Rated " + movie.Rated).attr("class", "p-rated");
+            var movieCast = $("<p>").html("Starring | " + movie.Actors).attr("class", "p-cast");
+            var moviePlot = $("<p>").html(movie.Plot).attr("class", "p-plot");
+            var movieTrailer = $("<a>").attr("href", "https://www.youtube.com/results?search_query=" + movie.Title + "+trailer").attr("target", "_blank").html("View trailers here");
+            var linkSeparator = $("<div>");
+            var movieStreams = $("<a>").attr("href", "https://www.justwatch.com/us/search?q=" + movie.Title).attr("target", "_blank").html("Where to find it");
+    
+            movieRow.append(movieCol.append(movieCard));
+            movieCard.append(movieCardHeader.append(movieName));
+            movieCard.append(movieCardBody);
+            movieCardBody.append(movieRating);
+            movieCardBody.append(movieCast);
+            movieCardBody.append(moviePlot);
+            movieCardBody.append(movieTrailer);
+            movieCardBody.append(linkSeparator);
+            movieCardBody.append(movieStreams);
+    
+    
+          
+          })
         }
-    });
-
+      });
 });
 
-function getMovieDetails(title) {
-
-  $(".movie-col").remove();
-
-    var param = $.param({
-        t: title,
-        plot: "short",
-        apikey: "dadc91b3"
-    });
-
-    $.ajax({
-        url:"https://www.omdbapi.com/?" + param,
-        method: "GET", 
-    }).then(function(movie) {
-
- 
-      console.log("is this working?");
-        var movieRow = $(".movie-row");
-        var movieCol = $("<div>").attr("class", "col-lg-4 movie-col");
-        var movieCard = $("<div>").attr("class", "card movie-card");
-        var movieCardHeader = $("<div>").attr("class", "card-header movie-card-header");
-        var movieCardBody = $("<div>").attr("class", "card-body movie-card-body");
-
-        var movieName = $("<h4>").html(movie.Title).attr("class", "movie-title");
-        var movieRating = $("<p>").html("Rated " + movie.Rated).attr("class", "p-rated");
-        var movieCast = $("<p>").html("Starring | " + movie.Actors).attr("class", "p-cast");
-        var movieScore = $("<p>").html(movie.Ratings[1].Source + " | " + movie.Ratings[1].Value).attr("class", "p-score");
-        var moviePlot = $("<p>").html(movie.Plot).attr("class", "p-plot");
-        var movieTrailer = $("<a>").attr("href", "https://www.youtube.com/results?search_query=" + movie.Title + "+trailer").attr("target", "_blank").html("View trailers here");
-        var linkSeparator = $("<div>");
-        var movieStreams = $("<a>").attr("href", "https://www.justwatch.com/us/search?q=" + movie.Title).attr("target", "_blank").html("Where to find it");
-
-        movieRow.append(movieCol.append(movieCard));
-        movieCard.append(movieCardHeader.append(movieName));
-        movieCard.append(movieCardBody);
-        movieCardBody.append(movieRating);
-        movieCardBody.append(movieCast);
-        movieCardBody.append(movieScore);
-        movieCardBody.append(moviePlot);
-        movieCardBody.append(movieTrailer);
-        movieCardBody.append(linkSeparator);
-        movieCardBody.append(movieStreams);
 
 
 
-    })
-    
-};
-
-// getTitle();
 

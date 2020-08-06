8e41b0327ef7d98638039b7a6947df3afbedb026
Jim Faulkner <jhf1203@gmail.com>
2020-08-05 23:08:37 -0400

    resolved undefined issue

--- assets/script.js
+++ assets/script.js
@@ -127,7 +127,7 @@ $('#rest-btn').on('click', function(){
 //MOVIE GENERATOR
 
 
-$("#movie-btn").on("click", function () {
+$("#movie-btn").on("click", function findMovie() {
 
   // Assigning variables to our user-selected search criteria
   var startYear = $(".earliest-year-selector").val();
@@ -138,73 +138,86 @@ $("#movie-btn").on("click", function () {
   $.ajax({
       url: "https://api.themoviedb.org/3/discover/movie?api_key=183cf14b0fa970fabe87a2879d2f3aa1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=" + startYear + "-01-01&primary_release_date.lte=" + endYear + "-12-31&with_genres=" + genre + "&with_original_language=en",
       method: "GET",
-  }).then(function(response) {
-    // console.log(response.results);
+  }).then(function (response) {
+    console.log(response.results);
     
       // Ensuring we do not always populate the top three results, allowing the user to search multiple times if the three titles that populate do not meet their needs
 
     var j = Math.floor(Math.random() * (response.results.length - 4));
     
-      for (var i = j; i < (j + 3); i++) {
+    for (var i = j; i < (j + 3); i++) {
 
-        var tmdbFullYear = response.results[i].release_date;
-        var tmdbYearOnly = tmdbFullYear.substring(0,4);
-        var title = response.results[i].original_title;
+      console.log("We are starting at index " + j);
+
+      var tmdbFullYear = response.results[i].release_date;
+      var tmdbYearOnly = tmdbFullYear.substring(0,4);
+      var title = response.results[i].original_title;
       
       console.log("index " + i + " "  + title + " is the tmdb title");
       // Clearing out previous movies generated
-        $(".movie-col").remove();
+      $(".movie-col").remove();
 
         // Setting parameters for second ajax call (omdb)
-        var param = $.param({
-          t: title,
-          y: tmdbYearOnly,
-          plot: "short",
-          apikey: "dadc91b3"
-        });
+      var param = $.param({
+        t: title,
+        y: tmdbYearOnly,
+        plot: "short",
+        apikey: "dadc91b3"
+      });
     
-        $.ajax({
-            url:"https://www.omdbapi.com/?" + param,
-            method: "GET", 
-        }).then(function(movie) {
+      $.ajax({
+        url:"https://www.omdbapi.com/?" + param,
+        method: "GET", 
+      }).then(function(movie) {
 
-          console.log(movie);
+        console.log(movie);
+
+          // Sometimes the movie in tmdb won't be in the omdb database, or its title will be in a format that omdb won't recognize and therefore will return the movie as undefined.  This conditional restarts the function should that happen so that three movies fully populate.  
+
+        if (movie.Response === "False") {
+
+            // console.log("Here is the tmdb one messing us up " + title);
+            // console.log("And here is the omdb value which should say undefined" + movie.Title);
+          findMovie();
+           
+        }
     
-          console.log(movie.Title + " is the movie name in omdb");
+        console.log(movie.Title + " is the movie name in omdb");
           // Generating card elements for each movie chosen.
-            var movieRow = $(".movie-row");
-            var movieCol = $("<div>").attr("class", "col-lg-4 movie-col");
-            var movieCard = $("<div>").attr("class", "card movie-card");
-            var movieCardHeader = $("<div>").attr("class", "card-header movie-card-header");
-            var movieCardBody = $("<div>").attr("class", "card-body movie-card-body")
+        var movieRow = $(".movie-row");
+        var movieCol = $("<div>").attr("class", "col-lg-4 movie-col");
+        var movieCard = $("<div>").attr("class", "card movie-card");
+        var movieCardHeader = $("<div>").attr("class", "card-header movie-card-header");
+        var movieCardBody = $("<div>").attr("class", "card-body movie-card-body")
             
             // We're going to make something like line 183 work for the above.  Thinking maybe just set the poster as the body and then on hover show the other p-tags??  Currently I can't format the image without also formatting the text, so it's not readable.
 
-            // .css({"background-image":"url(" + movie.Poster + ")","background-size":"100%","filter":"grayscale(100%","opacity":"0.5"});
+            // .css({"background-image":"url(" + movie.Poster + ")","background-size":"100%"});
 
-            var movieName = $("<h4>").html(movie.Title).attr("class", "movie-title");
-            var movieRating = $("<p>").html("Rated " + movie.Rated).attr("class", "p-rated");
-            var movieCast = $("<p>").html("Starring | " + movie.Actors).attr("class", "p-cast");
-            var moviePlot = $("<p>").html(movie.Plot).attr("class", "p-plot");
-            var movieTrailer = $("<a>").attr("href", "https://www.youtube.com/results?search_query=" + movie.Title + "+trailer").attr("target", "_blank").html("View trailers here");
-            var linkSeparator = $("<div>");
-            var movieStreams = $("<a>").attr("href", "https://www.justwatch.com/us/search?q=" + movie.Title).attr("target", "_blank").html("Where to find it");
+        var movieName = $("<h4>").html(movie.Title).attr("class", "movie-title");
+        var movieRating = $("<p>").html("Rated " + movie.Rated).attr("class", "p-rated");
+        var movieCast = $("<p>").html("Starring | " + movie.Actors).attr("class", "p-cast");
+        var moviePlot = $("<p>").html(movie.Plot).attr("class", "p-plot");
+        var movieTrailer = $("<a>").attr("href", "https://www.youtube.com/results?search_query=" + movie.Title + "+trailer").attr("target", "_blank").html("View trailers here");
+        var linkSeparator = $("<div>");
+        var movieStreams = $("<a>").attr("href", "https://www.justwatch.com/us/search?q=" + movie.Title).attr("target", "_blank").html("Where to find it");
     
-            movieRow.append(movieCol.append(movieCard));
-            movieCard.append(movieCardHeader.append(movieName));
-            movieCard.append(movieCardBody);
-            movieCardBody.append(movieRating);
-            movieCardBody.append(movieCast);
-            movieCardBody.append(moviePlot);
-            movieCardBody.append(movieTrailer);
-            movieCardBody.append(linkSeparator);
-            movieCardBody.append(movieStreams);
-    
+        movieRow.append(movieCol.append(movieCard));
+        movieCard.append(movieCardHeader.append(movieName));
+        movieCard.append(movieCardBody);
+        movieCardBody.append(movieRating);
+        movieCardBody.append(movieCast);
+        movieCardBody.append(moviePlot);
+        movieCardBody.append(movieTrailer);
+        movieCardBody.append(linkSeparator);
+        movieCardBody.append(movieStreams);
     
           
-          })
-        }
-      });
+    
+          
+      })
+    }
+  });
 });
 
 
